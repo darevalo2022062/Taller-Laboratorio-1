3c329002922a8853539f1f34f13ed1652ceb893f
David <darevalo-2022062@kinal.edu.gt>
2024-01-26 19:53:15 -0600

    educacion

--- Assets/scripts/kinal.js
+++ Assets/scripts/kinal.js
@@ -1,11 +1,19 @@
 
 function mostrarOtraSeccion(idSeccion) {
     var seccionPrincipal = document.getElementById('principal');
-    var otraSeccion = document.getElementById(idSeccion);
-
-    otraSeccion.style.display = 'block';
     seccionPrincipal.style.display = 'none';
-
+    var otraSeccion = document.getElementById(idSeccion);
+    otraSeccion.style.display = 'block';
+    if (idSeccion != "Quienes-Somos") {
+        var quinesSomos = document.getElementById('Quienes-Somos');
+        quinesSomos.style.display = 'none';
+    } else if (idSeccion != "educacion"){
+        var quinesSomos = document.getElementById('educacion');
+        quinesSomos.style.display = 'none';
+    }
     
+
+
+
 }
 
